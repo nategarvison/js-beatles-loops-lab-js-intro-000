55aaa8736027033881329e2440ab52ebf97530b1
nategarvison <ngarvison@gmail.com>
2017-09-01 16:19:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   const array = []
 
   for (let i = 0; l = musicians.length; i < l; i++) {
-    array.push(`${musicians} plays ${instruments}`)
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 
   return array
